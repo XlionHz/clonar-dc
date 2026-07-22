d6d2c2bb78cfff125b02b246dbff5b4402da6fe9
XlionHz <xlionhz@gmail.com>
2026-07-22 05:08:43 -0300

    ui: remove obsolete light selector workaround

--- src/ClonarDC.Desktop/AlphaFixes.cs
+++ src/ClonarDC.Desktop/AlphaFixes.cs
@@ -1,7 +1,6 @@
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 using ClonarDC.Services;
 
 namespace ClonarDC;
@@ -24,31 +23,8 @@ public partial class MainWindow
             item.Margin = new Thickness(0);
         }
 
-        FixLicenseSelectorContrast();
         InitializeUpdateUi();
         LocalizationService.Apply(this);
         Pages.SelectedIndex = 0;
-    }
-
-    private void FixLicenseSelectorContrast()
-    {
-        var fieldBackground = new SolidColorBrush(Color.FromRgb(248, 250, 252));
-        var fieldForeground = new SolidColorBrush(Color.FromRgb(17, 24, 39));
-        var accent = new SolidColorBrush(Color.FromRgb(139, 92, 246));
-
-        LicenseBox.Background = fieldBackground;
-        LicenseBox.Foreground = fieldForeground;
-        LicenseBox.BorderBrush = accent;
-        LicenseBox.BorderThickness = new Thickness(1);
-        LicenseBox.Padding = new Thickness(10, 7, 10, 7);
-        LicenseBox.MinHeight = 42;
-
-        foreach (var item in LicenseBox.Items.OfType<ComboBoxItem>())
-        {
-            item.Background = fieldBackground;
-            item.Foreground = fieldForeground;
-            item.Padding = new Thickness(10, 8, 10, 8);
-            item.HorizontalContentAlignment = HorizontalAlignment.Left;
-        }
     }
 }
