08ce05d0fe4534c4785b25adee43b7005bb1dc7d
XlionHz <xlionhz@gmail.com>
2026-07-25 11:37:09 -0300

    fix: disambiguate vector brand paths

--- src/ClonarDC.Desktop/BrandPresentation.cs
+++ src/ClonarDC.Desktop/BrandPresentation.cs
@@ -3,6 +3,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Effects;
 using System.Windows.Shapes;
+using Path = System.Windows.Shapes.Path;
 
 namespace ClonarDC;
 
